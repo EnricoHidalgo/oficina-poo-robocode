295b7d7ab653dd7f3fe917a94781fa0cac649436
Enrico Hidalgo <143716820+EnricoHidalgo@users.noreply.github.com>
2025-06-02 08:22:04 -0300

    Create Atvd 03 - Do...While.java
    Laço de repetição Do...While em JAVA.

---
+++ Atvd 03 - Do...While.java
@@ -0,0 +1,25 @@
+// Atividade: 03
+// Autor: Enrico Hidalgo
+// KATA: Vowel Count
+// Data: 08/05/2025
+
+// While (laço de repetição)
+
+public class Vowels {
+    public static int getCount(String str) {
+        int count = 0;
+        int i = 0;
+        
+        if (str.length() == 0) return 0;
+        
+        while (i < str.length()) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+            i++;
+        }
+        
+        return count;
+    }
+}
